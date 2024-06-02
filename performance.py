180bc363d3806f5dc064ea78c7bf1fc012e07e6d
Roberto Bertelli <robbybertelli@gmail.com>
2024-06-02 11:58:45 +0200

    Tuned tests execution times
    Signed-off-by: Roberto Bertelli <robbybertelli@gmail.com>

--- performance.py
+++ performance.py
@@ -11,37 +11,26 @@ except ImportError:
     using_ulab = False
 
 
-# Funzione per misurare il tempo
 def timer(func, *args, **kwargs):
-    start = time.time()
+    start = time.time_ns()
     result = func(*args, **kwargs)
-    end = time.time()
-    return result, (end - start) * 1000 
+    end = time.time_ns()
+    return result, (end - start) / 1000000
 
-# Moltiplicazione di matrici
-def matrix_multiplication():
-    a = np.eye(100)
-    b = np.ones((100, 100))
+def matrix_multiplication(a, b):
     return np.dot(a, b)
 
-# Interpolazione
-def interpolation():
-    x = np.linspace(0, 10, 10)
+def interpolation(x, xi):
     y = np.sin(x)
-    xi = np.linspace(0, 10, 100)
     yi = np.interp(xi, x, y)
     return yi
 
-def fft():
-    x = np.linspace(0, 2 * np.pi, 128)
+def fft(x):
     y = np.sin(x)
     return np.fft.fft(y)
 
 # Risoluzione di sistemi di equazioni lineari
-def solve_linear_system():
-    a = np.eye(50)
-    b = np.linspace(0, 10, 50)
-
+def solve_linear_system(a, b):
     if using_ulab:
         res = scipy.linalg.cho_solve(a, b)
     else:  
@@ -49,16 +38,38 @@ def solve_linear_system():
         res = scipy.linalg.cho_solve(factor, b)
     return res
 
+def generate_positive_definite_matrix(n):
+    # Create a diagonal matrix with positive entries
+    A = np.diag(np.arange(1, n + 1))
+    return A
+
+
 if using_ulab:
     prefix = "[ULAB]"
 else:
     prefix = "[NUMPY]"
 
 tests = [
-    {"name": "matrix multiplication", "fun": matrix_multiplication},
-    {"name": "interpolation", "fun": interpolation},
-    {"name": "fft", "fun": fft},
-    {"name": "linear system", "fun": solve_linear_system},
+    {
+        "name": "matrix multiplication",
+        "fun": matrix_multiplication, 
+        "kwargs": { "a": np.full((200, 200), 21), "b": np.full((200, 200), 65)},
+    },
+    {
+        "name": "interpolation",
+        "fun": interpolation,
+        "kwargs": { "x": np.linspace(0, 200, 100000), "xi": np.linspace(0, 10, 100000)}
+    },
+    {
+        "name": "fft",
+        "fun" : fft,
+        "kwargs": { "x": np.linspace(0, 2 * np.pi, 65536)},
+    },
+    {
+        "name": "linear system",
+        "fun": solve_linear_system,
+        "kwargs": { "a": generate_positive_definite_matrix(800), "b": np.linspace(0, 10, 800)}
+    },
 ]
 
 
@@ -79,10 +90,10 @@ def main():
     with open(testfile, "w") as file:
         file.write("test name,id,time,valid\n")
         for t in tests:
-            n_of_tests = 1000
+            n_of_tests = 100
             result = None
             for i in range(n_of_tests):
-                new_result, operation_time = timer(t["fun"])
+                new_result, operation_time = timer(t["fun"], **t.get("kwargs", {}))
                 if result is None:
                     result = new_result
                 result_comparison = result == new_result
